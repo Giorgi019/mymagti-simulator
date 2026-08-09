bcd10aa6b8e7028ec0c3f6fb052119b3d4122dbe
Giorgi019 <dekanoidze07@gmail.com>
2026-08-09 09:19:23 +0400

    პროექტის საწყისი ინიციალიზაცია და ბაზის კონფიგურაცია

---
+++ src/main/java/com/giorgi/mymagti_simulator/MymagtiSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.giorgi.mymagti_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymagtiSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymagtiSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giorgi/mymagti_simulator/MymagtiSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giorgi.mymagti_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymagtiSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
